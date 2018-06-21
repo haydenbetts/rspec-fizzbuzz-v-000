5cc00ad5e3413f67cae2b115b4ff5448d48e22d9
Hayden Betts <haydenbetts@gmail.com>
2018-06-21 15:43:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+# you could also solve this ~slightly~ more elegantly
+# by defining an empty response string and then += fizz and Buzz
+# to it
+
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
